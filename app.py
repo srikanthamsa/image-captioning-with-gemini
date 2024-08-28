6f7cf145570fc19b463e4f8397ef4957846ab617
Srikant <srikanthamsa@gmail.com>
2024-08-28 16:05:12 +0530

    fix: Bind Flask app to 0.0.0.0 to make it accessible externally

--- app.py
+++ app.py
@@ -47,4 +47,4 @@ def upload_file():
         return jsonify({'message': 'File successfully uploaded', 'tags': tags}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+     app.run(debug=True, host='0.0.0.0', port=5000)
